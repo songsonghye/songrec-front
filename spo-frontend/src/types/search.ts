a847ba88c0eebb17fec6a0486cfe36213608a8fe
hyewon <khw3480000@naver.com>
2026-03-27 16:53:38 +0900

    frontend

--- spo-frontend/src/types/search.ts
+++ spo-frontend/src/types/search.ts
@@ -1,3 +1,5 @@
+import type { SpotifyTrack } from './track'
+
 export interface MyTrack {
   trackId: number
   spotifyTrack: SpotifyTrack
@@ -7,4 +9,8 @@ export interface Artist {
   artistId?: string
   images?: Image[]
   name: string
+}
+
+export interface Image {
+  url: string
 }
