5c7cb29fe9d95c99a8d76ae125d54ca4fe3916fe
hyewon <khw3480000@naver.com>
2026-03-28 00:19:34 +0900

    서버 url 수정

--- spo-frontend/src/shared/api/client.ts
+++ spo-frontend/src/shared/api/client.ts
@@ -1,15 +1,15 @@
-import axios from 'axios'
+import axios from "axios";
 
-const apiUrl = import.meta.env.VITE_API_URL
+const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
 // 일반 api (Authorization 필요)
 export const api = axios.create({
   baseURL: apiUrl,
   withCredentials: true,
-})
+});
 
 // 인증 전용 api
 export const authApi = axios.create({
   baseURL: apiUrl,
   withCredentials: true,
-})
+});
