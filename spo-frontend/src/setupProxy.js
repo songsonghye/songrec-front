c784aab2b60a56a08ced91ede06775622553e268
hyewon <khw3480000@naver.com>
2026-03-27 22:38:44 +0900

    깃 꼬인거 해결

--- spo-frontend/src/setupProxy.js
+++ spo-frontend/src/setupProxy.js
@@ -1,11 +1,11 @@
-const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware')
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://localhost:8080',	// 서버 URL or localhost:설정한포트번호
+      target: 'http://3.34.189.161/', // 서버 URL or localhost:설정한포트번호
       changeOrigin: true,
     })
-  );
-};
+  )
+}
